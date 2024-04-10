298bcd2dc92cedb37963a8e2506a13363c70d3d7
urvangkothari87 <134337811+urvangkothari87@users.noreply.github.com>
2024-04-09 21:12:41 -0500

    Update streamlit_app.py
    added .json()

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,6 @@ if ingredients_list:
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
     
 
